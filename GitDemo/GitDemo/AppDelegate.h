1cd5b27275d3b0eec260527138fc50cdf18b9f58
qianfeng001 <897126758@qq.com>
2015-10-12 11:10:52 +0800

    init commit

---
+++ GitDemo/GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by qianfeng001 on 15/10/12.
+//  Copyright (c) 2015年 王冬冬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by qianfeng001 on 15/10/12.
+//  Copyright (c) 2015年 王冬冬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
